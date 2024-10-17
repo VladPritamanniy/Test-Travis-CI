08a07873b48444e76e9d889a65a920eb3b5afe1d
v_prytamannyi <pritamanny255@gmail.com>
2024-10-17 21:35:44 +0300

    Added minimal API and tests

---
+++ MyWebApp.Tests/ApiTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MyWebApp;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace MyWebApp.Tests
+{
+    public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ApiTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetHome_ReturnsSuccessStatusCode()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
