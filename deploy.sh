c5b4735ed4711be66e3e9d1d4b7c9250997012df
v_prytamannyi <pritamanny255@gmail.com>
2024-10-18 15:02:42 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
-docker tag vladprytamannyi/mywebapp:1.0 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/mywebapp
+docker tag vladprytamannyi/mywebapp:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/mywebapp
 
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/mywebapp
