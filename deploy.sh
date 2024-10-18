e5d251ce461b81af7d87dd451fe8ca30569ff30b
v_prytamannyi <pritamanny255@gmail.com>
2024-10-18 14:24:31 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-REGION="eu-north-1"
-REPOSITORY_NAME="mywebapp"
-ACCOUNT_ID="904233130767"
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 904233130767.dkr.ecr.eu-north-1.amazonaws.com
 
-$(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
+docker tag vladprytamannyi/mywebapp:1.0 904233130767.dkr.ecr.eu-north-1.amazonaws.com/mywebapp
 
-aws ecr create-repository --repository-name $REPOSITORY_NAME || true
-
-docker tag mywebapp:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
-docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
+docker push 904233130767.dkr.ecr.eu-north-1.amazonaws.com/mywebapp
